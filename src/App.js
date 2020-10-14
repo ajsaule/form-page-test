cb6f2e0114a6a26e6eb9b07dd2db5f56c3551e15
andrej <andrejsaule8@gmail.com>
2020-10-14 17:10:45 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="container">
+          <nav>
+            <h1>LOREM IPSUM</h1>
+            <ul>
+              <Link to="/">
+                <li>Home</li>
+              </Link>
+              <Link to="/about">
+                <li>About</li>
+              </Link>
+            </ul>
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/about" component={About} /> 
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.scss';
+
+export default function About() {
+
+    return (
+        <div>
+            <p>Test test test test </p>
+        </div>
+    )
+}
